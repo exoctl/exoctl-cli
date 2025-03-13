99fccde55c88c8d5a5d33ee03784509c19b48c5d
VitorMob <mobhacking100@gmail.com>
2025-03-13 17:57:28 -0300

    feat: draft (requests)

--- src/infinity-cli.lua
+++ src/infinity-cli.lua
@@ -1,5 +1,7 @@
 local Config    = require("src.config")
 local App    = require("src.app")
+local Requests = require("src.gateways.web.requests")
 
 Config:setup("config/config.json")
 App:setup(Config)
+Requests:setup(Config)
